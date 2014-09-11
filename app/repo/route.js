a54dd66cf3364333767ed976722696d65f13310a
kamalakannan <nkamalakannan1992@gmail.com>
2014-09-11 06:23:23 +0530

    set current repo appropriately

--- app/repo/route.js
+++ app/repo/route.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model: function(params) {
-		return this.store.find('repo',params["owner.login"]+"/"+params["name"]);
-	}
+  model: function(params) {
+    return this.store.find('repo',params["owner.login"]+"/"+params["name"]);
+  },
+  afterModel: function(model) {
+    this.currentRepo.set('content',model);
+  }
 });
