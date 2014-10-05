e28b71658d559c49e2816ebfde149d4a6ae618e3
kamalakannan <nkamalakannan1992@gmail.com>
2014-10-05 14:44:27 +0530

    Add modal and modal trigger component from addon

---
+++ app/components/kk-modal-trigger.js
@@ -0,0 +1,3 @@
+import KkModalTrigger from 'ember-cli-kamalaknn-components/components/kk-modal-trigger/component';
+
+export default KkModalTrigger.extend();
---
+++ app/components/kk-modal.js
@@ -0,0 +1,3 @@
+import KkModal from 'ember-cli-kamalaknn-components/components/kk-modal/component';
+
+export default KkModal.extend();
